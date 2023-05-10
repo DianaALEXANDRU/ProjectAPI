43877e653bb5b4ee14da25b2b04a091b6726b544
Calcioiu Alexandru <77210765+Twa1x@users.noreply.github.com>
2023-05-10 21:09:42 +0300

    Update StockService.cs

--- Core/Services/StockService.cs
+++ Core/Services/StockService.cs
@@ -3,6 +3,7 @@ using DataLayer;
 using DataLayer.Dtos;
 using DataLayer.Entities;
 using DataLayer.Mapping;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,12 +43,17 @@ namespace Core.Services
             if (payload == null) return null;
 
 
+            var existingProduct = unitOfWork.Products.GetById(payload.ProductId);
+            if (existingProduct == null) return null;
+
+
             var newStock = new Stock
             {
                 Quantity = payload.Quantity,
                 Price = payload.Price,
                 ProductId = payload.ProductId,
-              
+                Product = existingProduct,
+
             };
 
             unitOfWork.Stock.Insert(newStock);
@@ -56,6 +62,52 @@ namespace Core.Services
             return payload;
         }
 
+        public bool DeleteById(int stockId)
+        {
+            var stock = unitOfWork.Stock.GetById(stockId);
+
+            if (stock == null)
+            {
+                return false;
+            }
+
+
+            unitOfWork.Stock.Remove(stock);
+            unitOfWork.SaveChanges();
+
+            return true;
+
+        }
+
+        public bool UpdateStock(UpdateStockDto updateStockDto)
+        {
+
+            var stock = unitOfWork.Stock.GetById(updateStockDto.Id);
+
+            if (stock == null)
+            {
+                return false;
+            }
+
+            if ( updateStockDto.Price >=0 )
+            {
+                stock.Price = updateStockDto.Price;
+            }
+            if (updateStockDto.Quantity >= 0)
+            {
+                stock.Quantity = updateStockDto.Quantity;
+            }
+           
+            if(unitOfWork.Products.GetById(updateStockDto.ProductId) != null)
+               stock.ProductId = updateStockDto.ProductId;
+            
+
+
+
+            unitOfWork.Stock.Update(stock);
+            unitOfWork.SaveChanges();
+            return true;
+        }
 
     }
 }
